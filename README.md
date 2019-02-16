e65075fcd1fcd1689ee274b9b4680da7f1fd3f05
Jennael <39685710+Jennael@users.noreply.github.com>
2019-02-16 09:50:33 +0800

    Added clarifying comments

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # 50.003-accenture-ticketing-project
+
+This is a web app that allows users to submit tickets, and admins to respond to them.
+
+###Current features:
+- sign in page
+
+###Future features:
+- database integration (sign in page now works)
+- ticket submission form
+- live chat feature
+- sms and email alerts
+
+###Dependencies:
+
+
+# Resources
+
+Tutorials:
+https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b
+
+Express: https://expressjs.com/en/starter/hello-world.html
+
+
+
+
+
--- ticketingapp-1.0.0/server.js
+++ ticketingapp-1.0.0/server.js
@@ -1,25 +1,41 @@
-//Import statement
+/*
+ * 16 Feb 2019
+ * Log in page for ticketing app
+ * 
+
+/* Requiring modules */
+const request = require('request'); //Request allows us make our API call and make an http request in node.
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser'); 
-const request = require('request'); 
 
+const app = express(); //Create an instance named app by invoking Express
 
+app.use(express.static('public')); //Access to files in 'public', where CSS file is located
+app.use(bodyParser.urlencoded({ extended: true })); //Body-parser allows us to make use of the key-value pairs stored on the req-body object
 
-app.use(express.static('public')); 
-app.use(bodyParser.urlencoded({ extended: true })); 
+/*
+EJS (Embedded JavaScript) allows us to interact with variables and then dynamically 
+create our HTML based on those variables. In order to use EJS in Express, we need 
+to set up our template engine. 
+EJS is accessed by default in the views directory.
+*/
 app.set('view engine', 'ejs')
 
-
+/* 
+If we visit the root URL, Express will respond with a rendered page of index.ejs. 
+*/
 app.get('/', function (req, res) {
     res.render('index')
 })
 
+/*
+Post to the root URL (/). 
+*/
 app.post('/', function (req, res) {
-    let user = req.body.user;
-    let pw = req.body.pw;
+    let user = req.body.user; //Refer to index.ejs (html), 'body' block, name 'user'
+    let pw = req.body.pw; //Refer to index.ejs (html), 'body' block, name 'pw'
 
-    console.log(user, pw);
+    console.log(user, pw); //Log data to console
 
     /*
     //API call
@@ -44,8 +60,11 @@ app.post('/', function (req, res) {
 })
 
 
-
-app.listen(3000, function () {
+/*
+Server is listening on port 3000 for connections. 
+To access web app, go to (http://localhost:3000)
+*/
+app.listen(3000, function () { 
   console.log('Example app listening on port 3000!')
 })
 
